d43e75d6e88e721574428d4923360ee68c16bd97
vincent <omegaortiz1997@gmail.com>
2018-01-29 20:39:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var name = "Vincent"
 var height = 80
 var message = "And will be the best programmer at school."
-var Display = `${name} is ${height} inches tall.` <br> `${message}`
+var Display = `${name} is ${height} inches tall. ${message}`
 module.exports = {name, height, Display, message}
 
 
